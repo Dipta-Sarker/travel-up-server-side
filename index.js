5d1d3f2bc1d26e1352f81a3c026ec9cbb692847b
Dipta-Sarker <contact.diptasarker@gmail.com>
2022-12-20 06:59:56 +0600

    get api worked

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const app = express()
 var cors = require('cors')
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
+require('dotenv').config()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+const uri = `mongodb+srv://${process.env.APP_USER}:${process.env.APP_PASSWORD}@cluster0.rxjlfn3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+        const travelCollection = client.db('travelUp').collection('services')
+
+        app.get('/services', async(req,res)=>{
+            const query ={}
+            const cursor = travelCollection.find(query)
+            const result = await cursor.limit(3).toArray()
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(e=>console.error(e))
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
